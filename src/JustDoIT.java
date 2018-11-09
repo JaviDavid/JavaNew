b3eb94bf7b05d18e7ace23038771ddd52e00dfb7
JaviDavid <Gradinescu7!>
2018-11-09 19:28:36 +0200

    Import

--- src/JustDoIT.java
+++ src/JustDoIT.java
@@ -1,6 +1,9 @@
+import java.sql.SQLOutput;
+
 public class JustDoIT {
     public static void main(String[] args) {
         int myAge = 32;
+        int anAge = myAge;
         float weight = 60.5f;
         double height = 356.6;
         long distance = 11299098877888L;
@@ -9,8 +12,16 @@ public class JustDoIT {
         boolean isStudent = false;
         boolean hasJob = true;
         int test2 = (int)weight;
-        System.out.print(test);
-        System.out.print(test2);
+
+        String mesaj1 = "Hello";
+        String mesaj2 = "GoodBye";
+        String rezultat = mesaj1+mesaj2;
+        System.out.println(rezultat);
+        double a = 5;
+        int b = 2;
+        double c = a%b;
+
+
 
 
 
