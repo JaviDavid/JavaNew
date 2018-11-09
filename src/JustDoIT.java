9c944162534d4d6476efef17640df52dd2fb11b2
JaviDavid <Gradinescu7!>
2018-11-09 18:08:28 +0200

    Import

---
+++ src/JustDoIT.java
@@ -0,0 +1,12 @@
+public class JustDoIT {
+    public static void main(String[] args) {
+        int myAge = 32;
+        float weight = 60.5f;
+        double height = 356.6;
+        long distance = 11299098877888L;
+        char gender = 'm';
+        char test = 9;
+        boolean isStudent = false;
+
+    }
+}
